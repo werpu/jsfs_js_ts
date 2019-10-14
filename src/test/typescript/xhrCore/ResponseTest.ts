2c447565030cee95f4f9a7abf7dcd84d8637847b
Werner Punz <werner.punz@gmail.com>
2019-10-14 15:38:33 +0200

    adding placeholders for the response tests.

---
+++ src/test/typescript/xhrCore/ResponseTest.ts
@@ -0,0 +1,40 @@
+import {describe} from "mocha";
+
+/**
+ * response test
+ * the idea is simply to pass in a dom
+ * the context and a response xml and then check what happens
+ * we do not need to to through the entire ajax cyle for that.
+ */
+describe('Tests of the various aspects of the response functionality', function () {
+
+    it("must have a simple field updated as well as the viewstate", () => {
+
+    });
+
+    it("must have a full body update", () => {
+
+    });
+
+    it("must have am embedded script properly executed", () => {
+
+    });
+
+    it("must have processed a proper insertBefore", () => {
+
+    });
+
+    it("must have processed a proper insertAfter", () => {
+
+    });
+
+    it("must have processed a proper delete", () => {
+
+    });
+
+    it("must have processed a proper eval of a script given in the eval tag", () => {
+
+    });
+
+    //TODO update head all and redirect
+});

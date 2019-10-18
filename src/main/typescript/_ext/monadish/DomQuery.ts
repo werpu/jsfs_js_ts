1acec850f1dae945ea51f9ef487a5350797f43a3
Werner Punz <werner.punz@gmail.com>
2019-10-18 14:11:57 +0200

    adding todo for the dom parser part

--- src/main/typescript/_ext/monadish/DomQuery.ts
+++ src/main/typescript/_ext/monadish/DomQuery.ts
@@ -300,7 +300,7 @@ export class DomQuery {
             let lowerMarkup = markup.toLowerCase();
             if (lowerMarkup.includes('<!doctype') ||
                 lowerMarkup.includes('<html')  ||
-                lowerMarkup.includes('<head')  ||
+                lowerMarkup.includes('<head')  || //TODO proper regexps here to avoid embedded tags with same element names to be triggered
                 lowerMarkup.includes('<body')) {
                 doc.documentElement.innerHTML = markup;
                 return new DomQuery(doc.documentElement);
@@ -308,7 +308,6 @@ export class DomQuery {
                 doc.body.innerHTML = markup;
                 return new DomQuery(... <Array<Element>> Lang.instance.objToArray(doc.body.childNodes));
             }
-
         }
     }
 

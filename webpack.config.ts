7f9eef9110ec6698c4831197f36ccb777c4feb1b
werpu <werner.punz@gmail.com>
2019-11-26 08:51:20 +0100

    adding development build for easier debugging

--- webpack.config.ts
+++ webpack.config.ts
@@ -5,54 +5,63 @@ import * as path from 'path'
 let BrotliPlugin = require('brotli-webpack-plugin');
 let CompressionPlugin = require('compression-webpack-plugin');
 
+/**
+ * we need to define the export in a function
+ * because the mode parameter is passed down via the argv
+ *
+ * @param env the environment
+ * @param argv the arguments list
+ */
+function build(env, argv) {
 
-let libraryTarget = process.env.TARGET_TYPE || "window";
+    let libraryTarget = env.TARGET_TYPE ?? "window";
 
-const config: webpack.Configuration = {
-    context: __dirname,
-    entry: "./src/main/typescript/api/Jsf.ts",
-    devtool: false,
-    output: {
-        path: path.resolve(__dirname, './dist/' + libraryTarget),
-        libraryTarget: libraryTarget,
-        filename: "jsf.js"
-    },
-    resolve: {
-        extensions: [".tsx", ".ts", ".js", ".json"]
-    },
-    module: {
-        rules: [
-            // all files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'
-            {
-                test: /\.tsx?$/, use: [{
-                    loader: "ts-loader"
-                }], exclude: /node_modules/
-            }
+    const config: webpack.Configuration = {
+        context: __dirname,
+        entry: "./src/main/typescript/api/Jsf.ts",
+        devtool: false,
+        output: {
+            path: path.resolve(__dirname, './dist/' + libraryTarget),
+            libraryTarget: libraryTarget,
+            filename: (argv.mode == "production") ? "jsf.js" : "jsf-development.js"
+        },
+        resolve: {
+            extensions: [".tsx", ".ts", ".js", ".json"]
+        },
+        module: {
+            rules: [
+                // all files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'
+                {
+                    test: /\.tsx?$/, use: [{
+                        loader: "ts-loader"
+                    }], exclude: /node_modules/
+                }
+            ]
+        },
+
+        plugins: [
+            new webpack.SourceMapDevToolPlugin({
+                filename: (argv.mode == "production") ? "jsf.js.map" : "jsf-development.js.map"
+            }),
+            //# sourceMappingURL=http://localhost:8080/IntegrationJSTest/javax.faces.resource/myfaces/api/main.js.map.jsf?ln=scripts
+            new CompressionPlugin({
+                filename: '[path].gz[query]',
+                algorithm: 'gzip',
+                test: /\.js$|\.css$|\.html$|\.eot?.+$|\.ttf?.+$|\.woff?.+$|\.svg?.+$/,
+                threshold: 10240,
+                minRatio: 0.3
+
+            }),
+            new BrotliPlugin({
+                asset: '[path].br[query]',
+                test: /\.(js|css|html|svg)$/,
+                threshold: 10240,
+                minRatio: 0.8
+            })
+
         ]
-    },
-
-    plugins: [
-        new webpack.SourceMapDevToolPlugin({
-            filename: "jsf.js.map"
-        }),
-        //# sourceMappingURL=http://localhost:8080/IntegrationJSTest/javax.faces.resource/myfaces/api/main.js.map.jsf?ln=scripts
-        new CompressionPlugin({
-            filename: '[path].gz[query]',
-            algorithm: 'gzip',
-            test: /\.js$|\.css$|\.html$|\.eot?.+$|\.ttf?.+$|\.woff?.+$|\.svg?.+$/,
-            threshold: 10240,
-            minRatio: 0.3
-
-        }),
-        new BrotliPlugin({
-            asset: '[path].br[query]',
-            test: /\.(js|css|html|svg)$/,
-            threshold: 10240,
-            minRatio: 0.8
-        })
-
-    ]
+    }
+    return config;
 }
-
-export default config;
+export default build;
 

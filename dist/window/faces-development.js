867c33a52229239cdcefecfdddf5b9baf994907e
Werner Punz <werner.punz@gmail.com>
2023-02-17 11:42:08 +0100

    jsdoc improvements

--- dist/window/faces-development.js
+++ dist/window/faces-development.js
@@ -4131,12 +4131,12 @@ var faces;
         /**
          * this function has to send the ajax requests
          *
-         * following requestInternal conditions must be met:
+         * following request conditions must be met:
          * <ul>
-         *  <li> the requestInternal must be sent asynchronously! </li>
-         *  <li> the requestInternal must be a POST!!! requestInternal </li>
-         *  <li> the requestInternal url must be the form action attribute </li>
-         *  <li> all requests must be queued with a client side requestInternal queue to ensure the requestInternal ordering!</li>
+         *  <li> the request must be sent asynchronously! </li>
+         *  <li> the request must be a POST!!! request </li>
+         *  <li> the request url must be the form action attribute </li>
+         *  <li> all requests must be queued with a client side request queue to ensure the request ordering!</li>
          * </ul>
          *
          * @param {String|Node} element: any dom element no matter being it html or jsf, from which the event is emitted
@@ -4152,7 +4152,6 @@ var faces;
          * @param request the request object having triggered this response
          * @param context the request context
          *
-         * TODO add info on what can be in the context
          */
         function response(request, context) {
             AjaxImpl_1.Implementation.response(request, context);
@@ -4167,10 +4166,10 @@ var faces;
          *     <li> errorData.status : the error status message</li>
          *     <li> errorData.serverErrorName : the server error name in case of a server error</li>
          *     <li> errorData.serverErrorMessage : the server error message in case of a server error</li>
-         *     <li> errorData.source  : the issuing source element which triggered the requestInternal </li>
-         *     <li> eventData.responseCode: the response code (aka http requestInternal response code, 401 etc...) </li>
-         *     <li> eventData.responseText: the requestInternal response text </li>
-         *     <li> eventData.responseXML: the requestInternal response xml </li>
+         *     <li> errorData.source  : the issuing source element which triggered the request </li>
+         *     <li> eventData.responseCode: the response code (aka http request response code, 401 etc...) </li>
+         *     <li> eventData.responseText: the request response text </li>
+         *     <li> eventData.responseXML: the request response xml </li>
          * </ul>
          *
          * @param errorListener error handler must be of the format <i>function errorListener(&lt;errorData&gt;)</i>
@@ -4367,11 +4366,15 @@ var Implementation;
      a) Monad like structures for querying because this keeps the code denser and adds abstractions
      that always was the strong point of jquery and it still is better in this regard than what ecmascript provides
     
-     b) Streams and lazystreams like java has, a pull like construct, ecmascript does not have anything like Lazystreams.
+     b) Streams and lazy streams like java has, a pull stream construct, ecmascript does not have anything like it.
+     (it has array filters and maps, but ES2015 does not support flatMap)
      Another option would have been rxjs but that would have introduced a code dependency and probably more code. We might
-     move to RXJS if the need arises however. But for now I would rather stick with my small self grown library which works
+     move to RXJS if the need arises, however. But for now I would rather stick with my small self grown library which works
      quite well and where I can patch quickly (I have used it in several industrial projects, so it works well
      and is heavily fortified by unit tests (140 testcases as time of writing this))
+     The long term plan is to eliminate the Stream usage as soon as we can move up to ES2019 (adding the missing
+     functions as shims, is a no go, because we are a library, and absolutey do not Shim anything which can leak
+     into the global namespace!)
     
      c) A neutral json like configuration which allows assignments of arbitrary values with reduce code which then can be
      transformed into different data representations
@@ -4439,7 +4442,7 @@ var Implementation;
     /**
      * @return the project stage also emitted by the server:
      * it cannot be cached and must be delivered over the server
-     * The value for it comes from the requestInternal parameter of the faces.js script called "stage".
+     * The value for it comes from the request parameter of the faces.js script called "stage".
      */
     function getProjectStage() {
         var _a, _b, _c;

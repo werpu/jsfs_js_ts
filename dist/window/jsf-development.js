b7845b1701eaade4d28990be91bcb402c48d3e4e
Werner Punz <werner.punz@gmail.com>
2023-03-06 08:59:18 +0100

    build commmit before 4.0.0 tag

--- dist/window/jsf-development.js
+++ dist/window/jsf-development.js
@@ -1549,8 +1549,13 @@ class DomQuery {
     get cDATAAsString() {
         let TYPE_CDATA_BLOCK = 4;
         let res = this.asArray
-            .flatMap(item => item.childNodes.asArray)
-            .filter(item => { var _a, _b; return ((_b = (_a = item === null || item === void 0 ? void 0 : item.value) === null || _a === void 0 ? void 0 : _a.value) === null || _b === void 0 ? void 0 : _b.nodeType) == TYPE_CDATA_BLOCK; })
+            .flatMap(item => {
+            return item.childNodes.asArray;
+        })
+            .filter(item => {
+            var _a, _b;
+            return ((_b = (_a = item === null || item === void 0 ? void 0 : item.value) === null || _a === void 0 ? void 0 : _a.value) === null || _b === void 0 ? void 0 : _b.nodeType) == TYPE_CDATA_BLOCK;
+        })
             .reduce((reduced, item) => {
             var _a, _b, _c;
             reduced.push((_c = (_b = (_a = item === null || item === void 0 ? void 0 : item.value) === null || _a === void 0 ? void 0 : _a.value) === null || _b === void 0 ? void 0 : _b.data) !== null && _c !== void 0 ? _c : "");
@@ -1720,7 +1725,8 @@ class DomQuery {
      * @param filterDoubles filter out possible double elements (aka same markup)
      */
     concat(toAttach, filterDoubles = true) {
-        const ret = new DomQuery(...this.asArray.concat(toAttach.asArray));
+        let domQueries = this.asArray;
+        const ret = new DomQuery(...domQueries.concat(toAttach.asArray));
         // we now filter the doubles out
         if (!filterDoubles) {
             return ret;
@@ -1765,7 +1771,7 @@ class DomQuery {
                 continue;
             }
             let res = this.rootNode[cnt].querySelectorAll(selector);
-            nodes = nodes.concat(objToArray(res));
+            nodes = nodes.concat(...objToArray(res));
         }
         return new DomQuery(...nodes);
     }
@@ -1993,40 +1999,63 @@ exports.DQ$ = DomQuery.querySelectorAll;
  * Extended array
  */
 Object.defineProperty(exports, "__esModule", ({ value: true }));
-exports.Es2019Array = void 0;
+exports.Es2019Array = exports._Es2019Array = void 0;
 /**
  * Extended array which adds various es 2019 shim functions to the normal array
  * We must remap all array producing functions in order to keep
  * the delegation active, once we are in!
  */
-class Es2019Array extends Array {
+class Es2019Array_ extends Array {
     constructor(...another) {
         super(...another);
+        if (another._another) {
+            this._another = another._another;
+        }
+        else {
+            this._another = another;
+        }
         //for testing it definitely runs into this branch because we are on es5 level
-        if (!Array.prototype.flatMap) {
-            this.flatMap = (flatMapFun) => this._flatMap(flatMapFun);
-        }
-        if (!Array.prototype.flat) {
-            this.flat = (flatLevel = 1) => this._flat(flatLevel);
-        }
+        //if (!(<any>Array.prototype).flatMap) {
+        this.flatMap = (flatMapFun) => this._flatMap(flatMapFun);
+        //}
+        //if (!(<any>Array.prototype).flat) {
+        this.flat = (flatLevel = 1) => this._flat(flatLevel);
+        //}
+    }
+    map(callbackfn, thisArg) {
+        const ret = Array.prototype.map.call(this._another, callbackfn, thisArg);
+        return new _Es2019Array(...ret);
     }
     concat(...items) {
-        return new Es2019Array(...super.concat(...items));
+        const ret = Array.prototype.concat.call(this._another, ...items);
+        return new _Es2019Array(...ret);
     }
     reverse() {
-        return new Es2019Array(...super.reverse());
+        const ret = Array.prototype.reverse.call(this._another);
+        return new _Es2019Array(...ret);
     }
     slice(start, end) {
-        return new Es2019Array(...super.slice(start, end));
+        const ret = Array.prototype.slice.call(this._another, start, end);
+        return new _Es2019Array(...ret);
     }
     splice(start, deleteCount) {
-        return new Es2019Array(...super.splice(start, deleteCount));
+        const ret = Array.prototype.splice.call(this._another, start, deleteCount);
+        return new _Es2019Array(...ret);
     }
     filter(predicate, thisArg) {
-        return new Es2019Array(...super.filter(predicate, thisArg));
+        const ret = Array.prototype.filter.call(this._another, predicate, thisArg);
+        return new _Es2019Array(...ret);
     }
+    reduce(callbackfn, initialValue) {
+        const ret = Array.prototype.reduce.call(this._another, callbackfn, initialValue);
+        return ret;
+    }
+    /*reduceRight(callbackfn: (previousValue: T, currentValue: T, currentIndex: number, array: T[]) => T, initialValue: T): T {
+        const ret = Array.prototype.reduceRight.call(callbackfn, initialValue);
+        return ret;
+    }*/
     _flat(flatDepth = 1) {
-        return this._flatResolve(this, flatDepth);
+        return this._flatResolve(this._another, flatDepth);
     }
     _flatResolve(arr, flatDepth = 1) {
         //recursion break
@@ -2040,14 +2069,47 @@ class Es2019Array extends Array {
             res = res.concat(mapped);
         };
         arr.forEach(reFlat);
-        return new Es2019Array(...res);
+        return new exports.Es2019Array(...res);
     }
-    _flatMap(mapperFunction, noFallback = false) {
+    _flatMap(mapperFunction) {
         let res = this.map(item => mapperFunction(item));
         return this._flatResolve(res);
     }
 }
-exports.Es2019Array = Es2019Array;
+//let _Es2019Array = function<T>(...data: T[]) {};
+//let oldProto = Es2019Array.prototype;
+function _Es2019Array(...data) {
+    let ret = new Es2019Array_(...data);
+    let proxied = new Proxy(ret, {
+        get(target, p, receiver) {
+            if ("symbol" == typeof p) {
+                return target._another[p];
+            }
+            if (!isNaN(parseInt(p))) {
+                return target._another[p];
+            }
+            else {
+                return target[p];
+            }
+        },
+        set(target, property, value) {
+            target[property] = value;
+            target._another[property] = value;
+            return true;
+        }
+    });
+    return proxied;
+}
+exports._Es2019Array = _Es2019Array;
+;
+/**
+ * this is the switch between normal array and our shim
+ * the shim is only provided in case the native browser
+ * does not yet have flatMap support on arrays
+ */
+exports.Es2019Array = (Array.prototype.flatMap) ? function (...data) {
+    return data;
+} : _Es2019Array;
 
 
 /***/ }),
@@ -2125,6 +2187,7 @@ exports._global$ = _global$;
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.Lang = void 0;
 const Monad_1 = __webpack_require__(/*! ./Monad */ "./node_modules/mona-dish/src/main/typescript/Monad.ts");
+const Es2019Array_1 = __webpack_require__(/*! ./Es2019Array */ "./node_modules/mona-dish/src/main/typescript/Es2019Array.ts");
 /**
  * Lang helpers crossported from the apache myfaces project
  */
@@ -2218,7 +2281,7 @@ var Lang;
         //special condition array delivered no offset no pack
         if (obj instanceof Array && !offset && !pack)
             return obj;
-        return pack.concat(Array.prototype.slice.call(obj, offset));
+        return new Es2019Array_1.Es2019Array(...pack.concat(Array.prototype.slice.call(obj, offset)));
     }
     Lang.objToArray = objToArray;
     /**

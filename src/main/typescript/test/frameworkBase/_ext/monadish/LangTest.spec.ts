1752bd4635f43bdca124a7c571af11593c0371cb
werpu <werner.punz@gmail.com>
2025-12-22 23:43:26 +0100

    fixing build after audit updates

--- src/main/typescript/test/frameworkBase/_ext/monadish/LangTest.spec.ts
+++ src/main/typescript/test/frameworkBase/_ext/monadish/LangTest.spec.ts
@@ -20,6 +20,7 @@ import monadish from "mona-dish";
 const { Lang } = monadish;
 
 import { createRequire } from 'module';
+// @ts-ignore
 const require = createRequire(import.meta.url);
 
 const jsdom = require("jsdom");
